44443c1819ddf21daee2a6f86813868778782e12
Ilya Verenich <ilyavere@gmail.com>
2018-05-05 03:16:11 +1000

    add average infdicators #146

--- core/ClassifierFactory.py
+++ core/ClassifierFactory.py
@@ -10,7 +10,19 @@ from ClassifierWrapper import ClassifierWrapper
 
 def get_classifier(method, mode, max_features=None, n_estimators=None, learning_rate=None, random_state=None, min_cases_for_training=30, max_depth=None, subsample=None, colsample_bytree=None):
 
-    if method == "rf" and mode == "regr":
+    if method == "xgb" and mode == "regr":
+        return ClassifierWrapper(
+            cls=XGBRegressor(n_estimators=n_estimators, learning_rate=learning_rate, subsample=subsample,
+                                     max_depth=max_depth, colsample_bytree=colsample_bytree, n_jobs=4, random_state=random_state),
+            min_cases_for_training=min_cases_for_training, mode=mode)
+
+    elif method == "xgb" and mode == "class":
+        return ClassifierWrapper(
+            cls=XGBClassifier(n_estimators=n_estimators, learning_rate=learning_rate, subsample=subsample,
+                                     max_depth=max_depth, colsample_bytree=colsample_bytree, n_jobs=4, random_state=random_state),
+            min_cases_for_training=min_cases_for_training, mode=mode)
+
+    elif method == "rf" and mode == "regr":
         return ClassifierWrapper(
             cls=RandomForestRegressor(n_estimators=n_estimators, max_features=max_features, random_state=random_state),
             min_cases_for_training=min_cases_for_training, mode=mode)
@@ -38,18 +50,6 @@ def get_classifier(method, mode, max_features=None, n_estimators=None, learning_
     elif method == "dt" and mode == "class":
         return ClassifierWrapper(
             cls=DecisionTreeClassifier(max_depth=max_depth, max_features=max_features, random_state=random_state),
-            min_cases_for_training=min_cases_for_training, mode=mode)
-
-    elif method == "xgb" and mode == "regr":
-        return ClassifierWrapper(
-            cls=XGBRegressor(n_estimators=n_estimators, learning_rate=learning_rate, subsample=subsample,
-                                     max_depth=max_depth, colsample_bytree=colsample_bytree, n_jobs=4, random_state=random_state),
-            min_cases_for_training=min_cases_for_training, mode=mode)
-
-    elif method == "xgb" and mode == "class":
-        return ClassifierWrapper(
-            cls=XGBClassifier(n_estimators=n_estimators, learning_rate=learning_rate, subsample=subsample,
-                                     max_depth=max_depth, colsample_bytree=colsample_bytree, n_jobs=4, random_state=random_state),
             min_cases_for_training=min_cases_for_training, mode=mode)
 
     else:
