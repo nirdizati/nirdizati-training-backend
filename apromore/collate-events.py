d01751ad29e75b8f79274e44ce448604d701b9a0
raboczi <simon.raboczi@unimelb.edu.au>
2018-08-14 16:42:35 +1000

    Added scripts for use with Apromore.
    Importantly, remtime prediction is changed to be expressed in seconds rather than days.

---
+++ apromore/collate-events.py
@@ -0,0 +1,93 @@
+"""
+Copyright (c) 2016-2017 The Nirdizati Project.
+This file is part of "Nirdizati".
+
+"Nirdizati" is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation; either version 3 of the
+License, or (at your option) any later version.
+
+"Nirdizati" is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this program.
+If not, see <http://www.gnu.org/licenses/lgpl.html>.
+"""
+
+import sys
+import json
+import time
+from kafka import KafkaProducer, KafkaConsumer
+import os.path
+
+if len(sys.argv) != 6:
+    sys.exit("Usage: python {} bootstrap-server:port control-topic events-topic prefixes-topic delay".format(sys.argv[0]))
+
+bootstrap_server, control_topic, source_topic, destination_topic, delay = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], float(sys.argv[5])
+state_dir = "collator_state"
+case_id_field = 'concept:name'
+event_nr_field = 'event_nr'
+
+print("Collating events from \"{}\" into \"{}\" with control channel \"{}\" every {} seconds".format(source_topic, destination_topic, control_topic, delay))
+consumer = KafkaConsumer(source_topic, bootstrap_servers=bootstrap_server, auto_offset_reset='earliest')
+controlConsumer = KafkaConsumer(control_topic, bootstrap_servers=bootstrap_server, auto_offset_reset='earliest')
+producer = KafkaProducer(bootstrap_servers=bootstrap_server)
+
+active_logs = None
+
+""" This is a map keyed on string-valued case_id and containing sequences of event objects """
+log_cases = {}
+
+""" As events arrive on source_topic, collate case prefixes and forward them on destination_topic """
+while True:
+    rawControlMessage = controlConsumer.poll()
+    for topicPartition, controlMessages in rawControlMessage.items():
+        for controlMessage in controlMessages:
+            event = json.loads(controlMessage.value.decode())
+            " TODO: clear log_cases of any deleted logs "
+            active_logs = set(event)
+            print("Updated active logs to {}".format(active_logs))
+
+    rawMessage = consumer.poll(timeout_ms=1.0, max_records=1)
+    if not rawMessage.items():
+        time.sleep(delay)
+    for topicPartition, messages in rawMessage.items():
+        for message in messages:
+            event = json.loads(message.value)
+            log_id = event.get('log_id')
+            if active_logs is not None and not log_id in active_logs:
+                print("Discarding event from deleted monitor {}".format(log_id))
+            else:
+                case_id = event.get('case_attributes').get(case_id_field)
+                event_nr = int(event.get('event_attributes').get(event_nr_field))
+
+                if not os.path.isdir(state_dir):
+                    os.mkdir(state_dir)
+                dir_path = os.path.join(state_dir, str(log_id))
+                if not os.path.isdir(dir_path):
+                    os.mkdir(dir_path)
+                file_path = os.path.join(dir_path, case_id)
+                if not os.path.isfile(file_path):
+                    case_prefix = []
+                else:
+                    case_prefix = json.loads(open(file_path).read())
+                if event_nr is None or event_nr > len(case_prefix):
+                    case_prefix.append(event.get('event_attributes'))
+                    if event_nr is not None and event_nr != len(case_prefix):
+                        print("Event is labeled as {} but case prefix only has {} events".format(event_nr, len(case_prefix)))
+                    open(file_path, mode='w').write(json.dumps(case_prefix, indent="\t"))
+                    print("Collated event {} of case {}".format(len(case_prefix), case_id))
+                    for predictorId in event.get('predictors'):
+                        prediction_job = { "log_id":          log_id,
+                                           "case_id":         case_id,
+                                           "event_nr":        event_nr,
+                                           "predictor":       predictorId,
+                                           "case_attributes": event.get('case_attributes'),
+                                           "prefix":          case_prefix }
+                        producer.send(destination_topic, json.dumps(prediction_job).encode('utf-8'))
+                    time.sleep(delay)
+                else:
+                    print("Skipped collating event {} of case {}".format(len(case_prefix), case_id))
---
+++ apromore/predict.py
@@ -0,0 +1,138 @@
+"""
+Copyright (c) 2016-2017 The Nirdizati Project.
+This file is part of "Nirdizati".
+
+"Nirdizati" is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation; either version 3 of the
+License, or (at your option) any later version.
+
+"Nirdizati" is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this program.
+If not, see <http://www.gnu.org/licenses/lgpl.html>.
+"""
+
+import warnings
+warnings.filterwarnings('ignore')
+
+import pickle
+import sys
+from sys import argv
+import time
+
+import numpy as np
+import pandas as pd
+
+import json
+import kafka
+import tempfile
+import urllib
+
+if len(argv) != 6:
+    sys.exit("Usage: python {} bootstrap-server:port apromore-server:port control-topic prefixes-topic predictions-topic".format(argv[0]))
+
+bootstrap_server, apromore_server, control_topic, prefixes_topic, predictions_topic = argv[1], argv[2], argv[3], argv[4], argv[5]
+
+print("Making predictions using case prefixes from \"{}\" into \"{}\" with control channel \"{}\"".format(prefixes_topic, predictions_topic, control_topic))
+consumer = kafka.KafkaConsumer(prefixes_topic, group_id="predict", bootstrap_servers=bootstrap_server, auto_offset_reset='earliest')
+controlConsumer = kafka.KafkaConsumer(control_topic, bootstrap_servers=bootstrap_server, auto_offset_reset='earliest')
+producer = kafka.KafkaProducer(bootstrap_servers=bootstrap_server)
+
+active_logs = None
+predictor_cache = {}
+
+def getPredictor(predictor_id):
+    """ Fetch a predictor by id, either by downloading it from Apromore or from the local cache """
+    # if the pickle is cached, we're done
+    if predictor_id in predictor_cache:
+        return predictor_cache[predictor_id]
+
+    # if the pickle isn't cached, retrieve it from the Apromore server
+    print("Downloading predictor {} from Apromore".format(predictor_id))
+    with tempfile.NamedTemporaryFile() as f:
+        urllib.request.URLopener().retrieve("http://{}/predictiveMonitor/id?{}".format(apromore_server, predictor_id), f.name);
+        pipelines = pickle.load(f)
+        bucketer = pickle.load(f)
+        dataset_manager = pickle.load(f)
+
+        dtypes = {col: "str" for col in dataset_manager.dynamic_cat_cols + dataset_manager.static_cat_cols +
+              [dataset_manager.case_id_col, dataset_manager.timestamp_col]}
+        for col in dataset_manager.dynamic_num_cols + dataset_manager.static_num_cols:
+            dtypes[col] = "float"
+
+    predictor_cache[predictor_id] = pipelines, bucketer, dataset_manager, dtypes
+    return predictor_cache[predictor_id]
+
+""" As case prefixes arrive on prefixes_topic, execute each required predictor and forward the prediction to predictions_topic """
+while True:
+    rawControlMessage = controlConsumer.poll()
+    for topicPartition, controlMessages in rawControlMessage.items():
+        for controlMessage in controlMessages:
+            event = json.loads(controlMessage.value.decode())
+            " TODO: clear log_cases of any deleted logs "
+            active_logs = set(event)
+            print("Updated active logs to {}".format(active_logs))
+
+    rawMessage = consumer.poll(timeout_ms=1.0, max_records=1)
+    if not rawMessage.items():
+        time.sleep(1.0)
+    for topicPartition, messages in rawMessage.items():
+        for message in messages:
+            jsonValue = json.loads(message.value)
+            log_id = jsonValue["log_id"]
+            if active_logs is not None and not log_id in active_logs:
+                print("Discarding case prefix from deleted monitor {}".format(log_id))
+            else:
+                predictor = jsonValue["predictor"]
+                pipelines, bucketer, dataset_manager, dtypes = getPredictor(predictor)
+                tag = dataset_manager.label_col
+                """print("Tag: {}".format(tag))"""
+
+                jsonPrefix = jsonValue["prefix"]
+                for event in jsonPrefix:
+                    event[dataset_manager.case_id_col] = jsonValue["case_id"]
+                    for case_attribute in jsonValue["case_attributes"]:
+                        if not case_attribute in event:
+                            event[case_attribute] = jsonValue["case_attributes"][case_attribute]
+            
+                prefix = json.dumps(jsonPrefix)
+                """
+                print("-- INPUT --")
+                print(prefix)
+                """
+                test = pd.read_json(prefix, orient='records', dtype=dtypes)
+                test[dataset_manager.timestamp_col] = pd.to_datetime(test[dataset_manager.timestamp_col])
+
+                # get bucket for the test case
+                bucket = np.asscalar(bucketer.predict(test))
+
+                # select relevant classifier
+                if bucket not in pipelines:  # TODO fix this
+                    sys.exit("No matching model has been trained!")
+
+                else:
+                    # make actual predictions
+                    preds = pipelines[bucket].predict_proba(test)
+                    if preds.ndim == 1:  #regression
+                        preds = pd.DataFrame(preds.clip(min=0), columns=[dataset_manager.label_col])
+
+                    preds = preds.to_json(orient='records')
+
+                    # emit the predictions to the predictions_topic
+                    output = {
+                        "log_id":      log_id,
+                        "case_id":     jsonValue["case_id"],
+                        "event_nr":    len(jsonValue["prefix"]),
+                        "predictions": { tag: json.loads(preds)[0] }
+                    }
+                    print("-- OUTPUT --")
+                    print(json.dumps(output))
+                    """
+                    print(json.dumps({ "input": jsonPrefix, "output": output }))
+                    """
+                    producer.send(predictions_topic, json.dumps(output).encode('utf-8'))
